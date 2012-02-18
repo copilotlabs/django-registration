91702f2caf37d9563b7633477ad6fea7237fe737
Hike Danakian <hike@copilotlabs.com>
2012-02-17 17:47:41 -0800

    Initial activation-with-password changes to nameless backend

--- registration/backends/nameless/__init__.py
+++ registration/backends/nameless/__init__.py
@@ -3,8 +3,17 @@ from registration.backends.simple import SimpleBackend
 from registration.forms import RegistrationFormUsernameEmailMatch
 
 
-class NamelessBackend(SimpleBackend):
+class NamelessBackend(DefaultBackend):
 
     def get_form_class(self, request):
         return RegistrationFormUsernameEmailMatch
 
+    def post_activation_redirect(self, request, user):
+        """
+        Return the name of the URL to redirect to after successful
+        account activation and login.
+        """
+
+        new_user = authenticate(username=username, password=password)
+        login(request, new_user)
+        return ('/', (), {})
